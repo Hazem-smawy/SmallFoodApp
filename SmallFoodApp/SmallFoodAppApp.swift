18e19b87e4ebf0205e2cfeabe288e1cad1af2663
hazem-smawy <hazemsmawy@gmail.com>
2022-10-02 15:04:37 +0300

    Initial Commit

---
+++ SmallFoodApp/SmallFoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallFoodAppApp.swift
+//  SmallFoodApp
+//
+//  Created by hazem smawy on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SmallFoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
